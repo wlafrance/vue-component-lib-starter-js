746b8fdceae588ee4b3825a1f4aeae909fc4ac8a
Warren LaFrance <lafrance.warren@jcdev.org>
2020-07-10 08:05:44 -0500

    Needed to fix and add a few files before adding vuetify.  Added Vue Composition API

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
-undefined
+import Vue from "vue";
+import App from "./App.vue";
+//import router from "./router";
+//import store from "./store";
+//import vuetify from "./plugins/vuetify";
+import VueCompositionApi from "@vue/composition-api";
+import * as moment from "moment";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi,moment);
+
+new Vue({
+  //router,
+  //store,
+  //vuetify,
+  render: h => h(App)
+}).$mount("#app");
