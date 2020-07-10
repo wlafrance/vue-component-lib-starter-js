6094bb7a47e028b89c3033c1d2dc1006569aceb3
Warren LaFrance <lafrance.warren@jcdev.org>
2020-07-10 09:35:50 -0500

    Added ComponentV as a vue component...

---
+++ src/components/ComponentV.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="global-style component-b">
+  <v-btn @click="onClick">Accept
+     <v-icon dark right>mdi-checkbox-marked-circle</v-icon>
+  </v-btn>
+
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { VBtn, VIcon } from "vuetify/lib";
+
+
+export default Vue.extend({
+  name: "JcdcBtn",
+ 
+  components: {
+    VBtn,
+    VIcon
+  },
+
+  computed: {
+  
+  },
+    methods: {
+    onClick () {
+     alert('You clicked me')
+    }}
+});
+</script>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import ComponentA from './ComponentA.vue'
 import ComponentB from './ComponentB.vue'
+import ComponentV from './ComponentV.vue'
 
 import   "../assets/main.scss"
 
 export {
   ComponentA,
-  ComponentB
+  ComponentB,
+  ComponentV
 }
